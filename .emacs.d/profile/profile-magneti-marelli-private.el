c5f6673aa0afb79b3f735e1977fa8803715081ec
claude.tete@gmail.com <claude.tete@gmail.com@ae1306bd-257c-e295-da0e-dccbb14111c3>
2012-06-06 07:17:44 +0000

    do not put private file on svn

--- .emacs.d/profile/profile-magneti-marelli-private.el
+++
@@ -1,9 +0,0 @@
-(setq profile-username "Claude TETE")
-
-(setq calendar-latitude 46.84)
-(setq calendar-longitude 0.55)
-(setq calendar-location-name "Chatellerault, FR")
-
-(provide 'profile-magneti-marelli-private)
-
-;;; profile-magneti-marelli-private.el ends here
