d1bf6aaabccedf0f5c58ad87beea70a7b441be08
Claude TETE <claude.tete@gmail.com>
2019-03-22 17:22:27 +0100

    fix wrong custom file

--- .emacs.d/dotemacs/tqnr-custom.el
+++ .emacs.d/dotemacs/tqnr-custom.el
@@ -1,24 +1,3 @@
-
-
-;;; Code:
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-  )
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-  )
-
-
-(provide 'tqnr-custom)
-
-;; tqnr-custom.el ends here
 ;;; tqnr-custom.el --- a config file to put all custom option from `customize'
 
 ;; Copyright (c) 2006-2019 Claude Tete
@@ -50,3 +29,24 @@
 ;;
 ;; be careful it is the last loaded file, so everything emacs put in will
 ;; overwrite your other settings (to be sure I always empty this file.)
+
+
+;;; Code:
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+  )
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+  )
+
+
+(provide 'tqnr-custom)
+
+;; tqnr-custom.el ends here
