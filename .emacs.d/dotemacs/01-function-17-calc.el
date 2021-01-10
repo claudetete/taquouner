e884f024c701a1047e6fd73a3e1d9b836840f77a
Claude TETE <claude.tete@gmail.com>
2021-01-10 22:13:11 +0100

    update about calc function

--- .emacs.d/dotemacs/01-function-17-calc.el
+++ .emacs.d/dotemacs/01-function-17-calc.el
@@ -37,7 +37,8 @@
 (defun calc-hms-to-deg (hms-string)
   "Convert Degre-Hour-Minute-Second format into degres decimal."
   (interactive)
-  (let ((hms (split-string hms-str "[°′″NW ]" t)))
+
+  (let ((hms (split-string hms-str "[°\'\"NEWS ]" t)))
     (flet ((to-deg ()
              (string-to-number
                (calc-eval (format "deg(%s@ %s' %s\")" (pop hms) (pop hms) (pop hms)))
@@ -49,6 +50,39 @@
     )
   )
 
+(defun calc-deg-hm-toggle (input)
+  (interactive)
+  (let ((without_comma (s-replace "," "." input))
+         (deg (if (s-match "[NEWS°@ \.]")
+                (car (s-split "[NEWS°@ \.]" without_comma t))
+                ()
+        )
+           )
+         (min) (sec) (N-S) (E-W))
+    (if (s-match "[°\'\"NEWS @]" without_comma)
+      (if (s-match "[°@]")
+        (setq deg)
+        )
+           ())
+  ))
+
+(defun calc-get-direction (string)
+  (cond
+    ((s-contains "N" string) "N")
+    ((s-contains "E" string) "E")
+    ((s-contains "W" string) "W")
+    ((s-contains "S" string) "S")
+    ((s-contains "" string) "S")
+    )
+  )
+
+(defun calc-get-deg (string)
+  (if (s-match "[NEWS]" string)
+    (car (s-split "[NEWS°@ \.]" string t))
+    ()
+    )
+  )
+
 (defun calc-deg-to-hm (deg-string)
   "Convert Degree Decimal to Degre-Hour-Minute format."
   (interactive)
@@ -57,7 +91,13 @@
     )
   )
 
-(message (calc-eval "deg(46@ 10.99\")"))
+(calc-eval "46.42 % 1")
+(calc-eval (math-convert-units (calc-eval "46.42" 'raw)
+                               (calc-eval "hms" 'raw)))
+(calc-eval "hms(46.42)")
+(message (calc-eval 1 'top))
+
+(message (calc-eval "deg(46@ 30\' 30\")"))
 (message (calc-eval "hms(46.1833332)"))
 
 ;; in Calc ("u c" to convert units)
